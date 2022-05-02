eac3fe0aae1cbf2807b06d3f9c4d105e84ae17f7
Geraldine Claros Rios <clar801067@siigo.com>
2022-05-01 22:12:11 -0500

    NO DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -1,7 +1,9 @@
 type Size = '' | 's' | 'm' | 'l' | 'xl'
+
 class Product {
   constructor(public name: string = '', public price: number = 0, public size: Size = '') {}
   toString() {
+    //No DRY
     if (this.name.length <= 0) throw new Error('name is empty')
     if (this.price <= 0) throw new Error('price is empty')
     if (this.size.length <= 0) throw new Error('size is empty')
