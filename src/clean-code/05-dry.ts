8817dd61384e27dec8be7f070d46cbc844baccb6
Geraldine Claros Rios <clar801067@siigo.com>
2022-05-02 04:43:19 -0500

    aply dry in isProductReady

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -2,15 +2,38 @@ type Size = '' | 's' | 'm' | 'l' | 'xl'
 
 class Product {
   constructor(public name: string = '', public price: number = 0, public size: Size = '') {}
+  isProductReady(): boolean {
+    for (const key in this) {
+      switch (typeof this[key]) {
+        case 'string':
+          if ((<string>(<unknown>this[key])).length <= 0) throw Error(`${key} is empty`)
+          break
+        case 'number':
+          if (<number>(<unknown>this[key]) <= 0) throw Error(`${key} is zero`)
+          break
+        default:
+          throw Error(`${typeof this[key]} is not valid`)
+      }
+    }
+
+    return true
+  }
+
   toString() {
     //No DRY
-    if (this.name.length <= 0) throw new Error('name is empty')
-    if (this.price <= 0) throw new Error('price is empty')
-    if (this.size.length <= 0) throw new Error('size is empty')
+    // if (this.name.length <= 0) throw new Error('name is empty')
+    // if (this.price <= 0) throw new Error('price is empty')
+    // if (this.size.length <= 0) throw new Error('size is empty')
+
+    // for (const key in this) {
+    //   console.log(key, typeof this[key])
+    // }
+    if (!this.isProductReady()) return
+
     return `${this.name} cuesta ${this.price} en la talla ${this.size}`
   }
 }
 ;(() => {
-  const bluePants = new Product('blue pants')
+  const bluePants = new Product('blue pants', 3, 's')
   console.log(bluePants.toString())
 })()
